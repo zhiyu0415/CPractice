896e55346df8e52d3e539efc14b2bc263375a7a2
zhiyu0415 <93966100+zhiyu0415@users.noreply.github.com>
2024-03-17 17:39:49 +0800

    for loop

--- P3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-int num=0;
-scanf("%d", &num);
-printf("%d\n", num);
-}
---
+++ w1p3.c
@@ -0,0 +1,8 @@
+int main()  
+{  
+    for (int i = 1; i < 11; i++){  
+        printf("%d ", i);  
+    }  
+    printf("\n");  
+    return 0;  
+}  
